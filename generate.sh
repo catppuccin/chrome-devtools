282b4f27f19bcaa786514bb6e0d7e383d80227da
Toshit Chawda <r58playz@gmail.com>
2024-03-29 22:16:24 -0700

    theme inactive css rules, performance insights record, lighthouse

--- generate.sh
+++ generate.sh
@@ -5,8 +5,8 @@ for flavor in "latte" "frappe" "macchiato" "mocha"; do
   for accent in "rosewater" "flamingo" "pink" "mauve" "red" "maroon" "peach" "yellow" "green" "teal" "sky" "sapphire" "blue" "lavender"; do
     echo generating: $flavor $accent
     mkdir themes/${flavor}_${accent}
-    whiskers template/manifest.json.hbr $flavor -o themes/${flavor}_${accent}/manifest.json --override "accent=$(tc $accent)"
-    whiskers template/devtools.css.hbr $flavor -o themes/${flavor}_${accent}/devtools.css --override "accent=$accent"
+    whiskers template/manifest.json.hbr $flavor -o themes/${flavor}_${accent}/manifest.json --overrides "{\"accent\":\"$(tc $accent)\"}"
+    whiskers template/devtools.css.hbr $flavor -o themes/${flavor}_${accent}/devtools.css --overrides "{\"accent\":\"{{$accent}}\"}"
     cp template/devtools.html template/devtools.js themes/${flavor}_${accent}
   done
 done
--- themes/macchiato_red/devtools.js
+++ themes/macchiato_red/devtools.js
@@ -1,1 +1,3 @@
 fetch(chrome.runtime.getURL('devtools.css')).then(r=>r.text()).then(r=>chrome.devtools.panels.applyStyleSheet(r));
+
+var abc = "abc";
