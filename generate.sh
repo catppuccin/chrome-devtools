1f9e119ab786e0433ae0a7ff99f6a9e6741735af
Toshit Chawda <r58playz@gmail.com>
2024-04-27 16:11:41 -0700

    fix: update whiskers and icon, symlink files

--- generate.sh
+++ generate.sh
@@ -1,12 +1,11 @@
-tc() { set ${*,,} ; echo ${*^} ; }
 rm -r themes
 mkdir themes
 for flavor in "latte" "frappe" "macchiato" "mocha"; do
   for accent in "rosewater" "flamingo" "pink" "mauve" "red" "maroon" "peach" "yellow" "green" "teal" "sky" "sapphire" "blue" "lavender"; do
     echo generating: $flavor $accent
     mkdir themes/${flavor}_${accent}
-    whiskers template/manifest.json.hbr $flavor -o themes/${flavor}_${accent}/manifest.json --overrides "{\"accent\":\"$(tc $accent)\"}"
-    whiskers template/devtools.css.hbr $flavor -o themes/${flavor}_${accent}/devtools.css --overrides "{\"accent\":\"{{$accent}}\"}"
-    cp -r template/images template/devtools.html template/devtools.js themes/${flavor}_${accent}
+    ln -s ../../template/images ../../template/devtools.html ../../template/devtools.js themes/${flavor}_${accent}/
   done
 done
+whiskers template/manifest.json.tera
+whiskers template/devtools.css.tera
